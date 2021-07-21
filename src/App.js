09a92fba657ebfb9266c525a2f43c4a173302ae2
bogutskii <sl9site@gmail.com>
2021-07-20 17:44:58 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import {connect} from "react-redux";
 
-function App() {
+function App(props) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<h1>Boilerplate React-Redux</h1>
+        <ul>
+            {props.userList.map(user=> <li>{user}</li>)}
+        </ul>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+    userList: state.userList
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    // changeCreateModal: (value) =>
+    //   dispatch({
+    //     type: 'CHANGE_CREATE_MODAL',
+    //     payload: {
+    //       value: value,
+    //     },
+    //   }),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/components/redux/reducers.js
@@ -0,0 +1,13 @@
+const initialState = {
+userList: ['Admin', 'Guest', 'Moder']
+};
+
+const storeFn = (state = initialState, action) => {
+  switch (action.type) {
+
+    default:
+      return state;
+  }
+};
+
+export default storeFn;
